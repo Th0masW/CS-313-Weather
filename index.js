36117ebdd2f17e63c57a173cc95732ba02417e8c
Thomas Whitelock <trnthomas@live.com>
2018-11-27 21:02:06 -0500

    added api

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var cors = require('cors')
+var app = express()
 
 express()
+	app.options('*', cors()) // include before other routes
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
