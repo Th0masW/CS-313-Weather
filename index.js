3a99eca55c61fca253260a2883751fe120ec06c6
Thomas Whitelock <trnthomas@live.com>
2018-11-27 20:23:23 -0500

    added apicors

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const cors = require('cors');
-const express = require('express');
 let app = express();
 .use(cors());
 .options('*', cors());
