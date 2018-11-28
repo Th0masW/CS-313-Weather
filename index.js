463dd8f6aa04d9047aef6fa5816b68c91b231fb8
Thomas Whitelock <trnthomas@live.com>
2018-11-27 22:15:15 -0500

    added apicors

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const PORT = process.env.PORT || 5000
 var cors = require('cors')
 
 express()
-  .options('/weather', cors())
+  //.options('/weather', cors())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))  
   //Weather test page
+  .get('/weather', (req, res))
   .get('/weather', (req, res) => res.render('pages/weather'))
    // .get('/weather', fetchWeather);
 	
