7bef4e2aea2bf7035af04eb1fa6f997a844e9a32
Thomas Whitelock <trnthomas@live.com>
2018-11-28 10:00:40 -0500

    added api cors

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ var app = express()
 app.use(cors())
 
 //express()
- // .use(cors({ origin: 'https://www.ncdc.noaa.gov' }))
+  app.use(cors({ origin: 'https://www.ncdc.noaa.gov' }))
 
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
   app.get('/', (req, res) => res.render('pages/index'))  
   //Weather test page
- // app.get('/weather', (req, res) => res.render('pages/weather'))
+  app.get('/weather', (req, res) => res.render('pages/weather'))
   app.options('/weather', cors())
-  app.get('/weather', cors(), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+  
 
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`)) 
