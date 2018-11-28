031b362005fc03ee16c0164282db4cb4299ddf13
Thomas Whitelock <trnthomas@live.com>
2018-11-27 22:33:18 -0500

    added apicors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var cors = require('cors')
  //   credentials: true };
 	 
 express()
-  .use(cors({ origin: 'https://www.ncdc.noaa.gov' }));
+  .use(cors({ origin: 'https://www.ncdc.noaa.gov' }))
  // .use(cors(corsOptions))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
