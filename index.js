bc7aa364c94d1553e1aebdc3f7a2253725fc872b
Thomas Whitelock <trnthomas@live.com>
2018-11-27 20:18:28 -0500

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var cors = require('cors')
+
+var app = express()
+app.use(cors())
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
