b324a03ae6813fb1aec8e791ac1aa851090722af
Thomas Whitelock <trnthomas@live.com>
2018-11-27 22:06:41 -0500

    added apicors

--- index.js
+++ index.js
@@ -2,27 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const cors = require('cors')
-var app = express()
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  if ("OPTIONS" == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-});
-
-
 express()
-  .options('/weather', cors()) // include before other routes
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
