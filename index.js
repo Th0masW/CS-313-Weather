b205306d9d385faec6caf89487e917d487d9bbb9
Thomas Whitelock <trnthomas@live.com>
2018-11-27 22:12:12 -0500

    added apicors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 var cors = require('cors')
 
 express()
-  .options('*', cors())
+  .options('/weather', cors())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
@@ -15,10 +15,4 @@ express()
    // .get('/weather', fetchWeather);
 	
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-	function fetchWeather(request, response) {
-
-	var harCoded = "ad adasd";
-	var JSONdata = harCoded
 	
-	response.render('pages/weather', JSONdata)
-}
