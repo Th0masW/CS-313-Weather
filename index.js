9c0ac5f0efe6cf4903e1b8a009f1fd7337b96039
Thomas Whitelock <trnthomas@live.com>
2018-11-27 20:06:13 -0500

    added api

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index'))  
+  //Weather test page
+  .get('/weather', (req, res) => res.render('pages/weather'))
+   // .get('/weather', fetchWeather);
+	
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	function fetchWeather(request, response) {
+
+	var harCoded = "ad adasd";
+	var JSONdata = harCoded
+	
+	response.render('pages/weather', JSONdata)
+}
