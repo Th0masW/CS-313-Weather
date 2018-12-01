d4c7c19e529fbc7c973429e8c2181a62f2b5b079
Thomas Whitelock <trnthomas@live.com>
2018-12-01 17:53:27 -0500

    AJAX

--- index.js
+++ index.js
@@ -14,11 +14,8 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))  
   //Weather test page
- // .get('/weather', cors(corsOptions),(req, res) => res.render('pages/weather'))
-  .get('/weather', cors(corsOptions), (req, res) {
-	  res.render('pages/weather')
-  })
-   // .get('/weather', fetchWeather);
+  .get('/weather', cors(corsOptions),(req, res) => res.render('pages/weather'))
+ // .get('/weather', fetchWeather);
 	
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 	function fetchWeather(request, response) {
