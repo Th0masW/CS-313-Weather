f1671ea6b8c0562a1ac31185d32a9a297425d248
Thomas Whitelock <trnthomas@live.com>
2018-12-18 15:34:27 -0500

    Fixing CORS error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
+const rp = require('request-promise);
 
 const corsOptions = {
   origin: 'https://www.ncdc.noaa.gov'
@@ -19,6 +20,20 @@ express()
    .get('/states', cors(corsOptions), (req, res) => res.render('pages/states'))
    //.get('/states', getStates)
 
+    .get('/theWeather', function (req, res) {
+        console.log("button pushed");
+        let url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=NORMAL_MLY&datatypeid=MLY-TAVG-NORMAL&startdate=2010-07-01&enddate=2010-08-01&stationid=GHCND:USC00047821';
+        let options = {
+            uri: url,
+            headers: { "token": "zycPIftdPBYeXUhNBlkqmlbOjeCqvoWy" },
+            json: true
+        };
+        rp(options)
+            .then(function (response) {
+                res.send(response);
+            })
+    })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 	function fetchWeather(request, response) {
 
@@ -29,8 +44,3 @@ express()
 }
 
 
-function getStates() {
-
-    placeHolder = "Hi there";
-    return placeHolder;
-}
