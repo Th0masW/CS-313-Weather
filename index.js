27d21640761b480ad2f85042c8c689c4fa7dd0ce
Thomas Whitelock <trnthomas@live.com>
2018-12-18 15:45:08 -0500

    Fixing CORS error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
-const rp = require('request-promise);
+var rp = require('request-promise')
 
 const corsOptions = {
   origin: 'https://www.ncdc.noaa.gov'
