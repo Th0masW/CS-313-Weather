5a339fd0f8adc487960517275ba6869812eb25bd
Thomas Whitelock <trnthomas@live.com>
2018-12-18 21:35:00 -0500

    Fixing CORS error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
-//var rp = require('request-promise')
+var rp = require('request-promise')
 const http = require('http')
 
 const corsOptions = {
@@ -18,8 +18,19 @@ express()
   //Weather test page
   .get('/weather', cors(corsOptions),(req, res) => res.render('pages/weather'))
    // .get('/weather', fetchWeather);
-   .get('/states', cors(corsOptions), (req, res) => res.render('pages/states'))
+   //.get('/states', cors(corsOptions), (req, res) => res.render('pages/states'))
    //.get('/states', getStates)
+   .get('/states', getState)
+
+function getState(request, response) {
+    var postalType = request.query.postageType
+    var weight = request.query.weight
+
+    var JSONdata = doMath(postalType, weight)
+
+    response.render('pages/states', JSONdata)
+}
+
 
     .get('/theWeather', function (req, res) {
         console.log("******************************button pushed**********");
