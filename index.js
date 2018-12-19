02c979683907c98c84886f6f4963604283d144d0
Thomas Whitelock <trnthomas@live.com>
2018-12-18 21:44:44 -0500

    Fixing CORS error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
-var rp = require('request-promise')
+//var rp = require('request-promise')
 const http = require('http')
 
 const corsOptions = {
